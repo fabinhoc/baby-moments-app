f2bdd9f6bd5876889f3c9d140c0d9d6e3f34c2d4
Fabio Cruz <fabinhocosta1202@gmail.com>
2025-03-31 21:47:52 -0300

    feat: Try to resolve deploy erro in netlify

--- src-pwa/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ignorePatterns: ['src-pwa/.eslintrc.js'], // Ignorar o .eslintrc.js durante a análise
-  parserOptions: {
-    project: 'tsconfig.json', // Certifique-se de que está apontando para o tsconfig correto
-  },
-  // Outras configurações do ESLint...
-};
