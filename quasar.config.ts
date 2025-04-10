2768176206745bd122bbf6183ca656d11a560c16
Fabio Cruz <fabinhocosta1202@gmail.com>
2025-04-10 10:00:08 -0300

    feat: Create auth store for authetication and create rules for authenticated routes

--- quasar.config.ts
+++ quasar.config.ts
@@ -138,7 +138,7 @@ export default defineConfig((ctx) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify'],
+      plugins: ['Notify', 'LocalStorage'],
     },
 
     // animations: 'all', // --- includes all animations
