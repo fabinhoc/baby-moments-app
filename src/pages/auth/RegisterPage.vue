6e40863f4cf21209291bf5f4b2840e1c8cce1bd4
Fabio Cruz <fabinhocosta1202@gmail.com>
2025-04-16 10:03:06 -0300

    feat: Add Register page

---
+++ src/pages/auth/RegisterPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import FormRegister from 'src/components/auth/FormRegister.vue';
+
+defineOptions({
+  name: 'RegisterPage',
+});
+</script>
+
+<template>
+  <q-page class="row items-center justify-evenly" align="center">
+    <div class="col-md-3 col-sm-6 col-xs-10">
+      <q-card class="q-pa-lg">
+        <q-card-section>
+          <FormRegister />
+          <div class="q-mt-lg">
+            <q-btn @click="$router.push({ name: 'login' })" color="accent" flat>{{
+              $t('app.pages.register.hasAnAccount')
+            }}</q-btn>
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
