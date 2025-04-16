4b4e990c4b69f9f34c02f6d7b407e7a2d3fc949e
Fabio Cruz <fabinhocosta1202@gmail.com>
2025-04-16 09:59:07 -0300

    feat: Add methods for authentication flows.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,12 @@
 import { storeToRefs } from 'pinia';
 import useApi from 'src/composables/useApi';
 import { useAuthStore } from 'src/stores/auth.store';
+import type { ForgotPasswordDto } from 'src/types/dto/ForgotPassword.dto';
 import type { LoginDto } from 'src/types/dto/Login.dto';
+import type { RegisterDto } from 'src/types/dto/Register.dto';
 import type { UserType } from 'src/types/User.type';
 
-export default function usePlanService() {
+export default function useAuthService() {
   const authStore = useAuthStore();
   const { isLoggedIn } = storeToRefs(authStore);
 
@@ -18,7 +20,48 @@ export default function usePlanService() {
     return true;
   };
 
+  const me = async (token: string): Promise<void> => {
+    const { api } = useApi('user');
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    const { data } = await api.get('user').then((res) => res.data);
+    authStore.authenticate(data.user, data.token);
+  };
+
+  const logout = async (): Promise<void> => {
+    const { post } = useApi('logout');
+    await post<void>('');
+    authStore.logout();
+  };
+
+  const verifyEmail = async (url: string) => {
+    url = url.replace(process.env.API_URL as string, '');
+    const { get } = useApi(url);
+    return await get();
+  };
+
+  const register = async (payload: RegisterDto) => {
+    const { post } = useApi('/register');
+    const { data } = await post<{ data: { user: UserType; token: string } }>(payload);
+    authStore.authenticate(data.user, data.token);
+  };
+
+  const resendVerification = () => {
+    const { post } = useApi('email/verification-notification');
+    return post('');
+  };
+
+  const forgotPassword = (payload: ForgotPasswordDto) => {
+    const { post } = useApi('forgot-password');
+    return post(payload);
+  };
+
   return {
     login,
+    logout,
+    me,
+    verifyEmail,
+    register,
+    resendVerification,
+    forgotPassword,
   };
 }
